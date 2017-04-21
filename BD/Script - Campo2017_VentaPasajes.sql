5bce8c5f8e4ac3854de7d9ed34751c48fbf231f1
pabgermone <pabgermone@gmail.com>
2017-04-21 18:40:01 -0300

    Cambio en Script Agregado:   - Usuario   - Rol   - Permiso   - PermisoCompuesto   - RolPermiso

--- BD/Script - Campo2017_VentaPasajes.sql
+++ BD/Script - Campo2017_VentaPasajes.sql
@@ -35,11 +35,37 @@ create table VueloPasajero(vueloPasajero_vuelo varchar(10) not null,
 create table Paquete(paquete_id int not null primary key,
 					 paquete_descripcion varchar(140));
 
+create table Permiso(permiso_id int primary key not null,
+					 permiso_nombre varchar(50) not null);
+
+create table PermisoCompuesto(permisoPadre int not null,
+							  permisoHijo int not null,
+							  foreign key(permisoPadre) references Permiso(permiso_id),
+							  foreign key(permisoHijo) references Permiso(permiso_id));
+
+create table Rol(rol_id int primary key not null,
+				 rol_nombre varchar(50) not null)
+
+create table RolPermiso(rolPermiso_rol int not null,
+						rolPermiso_permiso int not null,
+						foreign key(rolPermiso_rol) references Rol(rol_id),
+						foreign key(rolPermiso_permiso) references Permiso(permiso_id));
+
+create table Usuario(usuario_id int primary key not null,
+					 usuario_userName varchar(50) not null,
+					 usuario_nombre varchar(50) not null,
+					 usuario_apellido varchar(50) not null,
+					 usuario_password varchar(50) not null,
+					 usuario_rol int,
+					 foreign key(usuario_rol) references Rol(rol_id));
+
 create table Venta(venta_id int not null primary key,
+				   venta_usuario int not null,
 				   venta_fecha date not null,
 				   venta_precio decimal(19,2) not null,
 				   venta_paquete int,
-				   foreign key(venta_paquete) references Paquete(paquete_id));
+				   foreign key(venta_paquete) references Paquete(paquete_id),
+				   foreign key(venta_usuario) references Usuario(usuario_id));
 
 create table VentaPasajero(ventaPasajero_venta int not null,
 						   ventaPasajero_pasajero int not null,
