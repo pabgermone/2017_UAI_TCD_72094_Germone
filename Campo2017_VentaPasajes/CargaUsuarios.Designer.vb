0078a8b26810dbc3db8c6f3dfae7323fa66ad4e2
Marcelo <Marcelo@HP>
2017-04-21 20:16:25 -0300

    Form nuevo   - Form de prueba para ABM usuarios

---
+++ Campo2017_VentaPasajes/CargaUsuarios.Designer.vb
@@ -0,0 +1,121 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class CargaUsuarios
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.txtUsername = New System.Windows.Forms.TextBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.txtPass = New System.Windows.Forms.TextBox()
+        Me.btnIngreso = New System.Windows.Forms.Button()
+        Me.btnRegistro = New System.Windows.Forms.Button()
+        Me.lblPassChange = New System.Windows.Forms.LinkLabel()
+        Me.SuspendLayout()
+        '
+        'txtUsername
+        '
+        Me.txtUsername.Location = New System.Drawing.Point(12, 28)
+        Me.txtUsername.Name = "txtUsername"
+        Me.txtUsername.Size = New System.Drawing.Size(236, 20)
+        Me.txtUsername.TabIndex = 0
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(12, 12)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(98, 13)
+        Me.Label1.TabIndex = 1
+        Me.Label1.Text = "Nombre de Usuario"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(12, 66)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(61, 13)
+        Me.Label2.TabIndex = 3
+        Me.Label2.Text = "Contraseña"
+        '
+        'txtPass
+        '
+        Me.txtPass.Location = New System.Drawing.Point(12, 82)
+        Me.txtPass.Name = "txtPass"
+        Me.txtPass.Size = New System.Drawing.Size(236, 20)
+        Me.txtPass.TabIndex = 2
+        '
+        'btnIngreso
+        '
+        Me.btnIngreso.Location = New System.Drawing.Point(12, 166)
+        Me.btnIngreso.Name = "btnIngreso"
+        Me.btnIngreso.Size = New System.Drawing.Size(111, 23)
+        Me.btnIngreso.TabIndex = 4
+        Me.btnIngreso.Text = "Ingresar"
+        Me.btnIngreso.UseVisualStyleBackColor = True
+        '
+        'btnRegistro
+        '
+        Me.btnRegistro.Location = New System.Drawing.Point(137, 166)
+        Me.btnRegistro.Name = "btnRegistro"
+        Me.btnRegistro.Size = New System.Drawing.Size(111, 23)
+        Me.btnRegistro.TabIndex = 5
+        Me.btnRegistro.Text = "Registrarse"
+        Me.btnRegistro.UseVisualStyleBackColor = True
+        '
+        'lblPassChange
+        '
+        Me.lblPassChange.AutoSize = True
+        Me.lblPassChange.LinkColor = System.Drawing.SystemColors.ControlText
+        Me.lblPassChange.Location = New System.Drawing.Point(75, 126)
+        Me.lblPassChange.Name = "lblPassChange"
+        Me.lblPassChange.Size = New System.Drawing.Size(107, 13)
+        Me.lblPassChange.TabIndex = 6
+        Me.lblPassChange.TabStop = True
+        Me.lblPassChange.Text = "Olvido su contraseña"
+        '
+        'CargaUsuarios
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(265, 201)
+        Me.Controls.Add(Me.lblPassChange)
+        Me.Controls.Add(Me.btnRegistro)
+        Me.Controls.Add(Me.btnIngreso)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.txtPass)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.txtUsername)
+        Me.Name = "CargaUsuarios"
+        Me.Text = "CargaUsuarios"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents txtUsername As TextBox
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents txtPass As TextBox
+    Friend WithEvents btnIngreso As Button
+    Friend WithEvents btnRegistro As Button
+    Friend WithEvents lblPassChange As LinkLabel
+End Class
---
+++ Campo2017_VentaPasajes/CargaUsuarios.vb
@@ -0,0 +1,3 @@
+﻿Public Class CargaUsuarios
+
+End Class
