5fcf20e8e1bcb4f0a8ddf9c7dba5d9e6383d3fcc
pabgermone <pabgermone@gmail.com>
2017-05-29 00:05:23 -0300

    Cambios en FormRoles   - Los roles se muestran en una lista y se ve un treeview con todos los permisos del sistema, donde se ven seleccionados los permisos que tiene el rol

--- Campo2017_VentaPasajes/Seguridad/Roles/FormRoles.Designer.vb
+++ Campo2017_VentaPasajes/Seguridad/Roles/FormRoles.Designer.vb
@@ -1,9 +1,9 @@
-﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
 Partial Class FormRoles
     Inherits System.Windows.Forms.Form
 
     'Form overrides dispose to clean up the component list.
-    <System.Diagnostics.DebuggerNonUserCode()> _
+    <System.Diagnostics.DebuggerNonUserCode()>
     Protected Overrides Sub Dispose(ByVal disposing As Boolean)
         Try
             If disposing AndAlso components IsNot Nothing Then
@@ -20,165 +20,117 @@ Partial Class FormRoles
     'NOTE: The following procedure is required by the Windows Form Designer
     'It can be modified using the Windows Form Designer.  
     'Do not modify it using the code editor.
-    <System.Diagnostics.DebuggerStepThrough()> _
+    <System.Diagnostics.DebuggerStepThrough()>
     Private Sub InitializeComponent()
-        Me.tableLayoutPanel1 = New System.Windows.Forms.TableLayoutPanel()
-        Me.lblNombreFamilia = New System.Windows.Forms.Label()
-        Me.lblTituloPatentes = New System.Windows.Forms.Label()
-        Me.panel1 = New System.Windows.Forms.Panel()
-        Me.Familia = New System.Windows.Forms.Label()
-        Me.txtNombre = New System.Windows.Forms.TextBox()
-        Me.treePatentes = New System.Windows.Forms.TreeView()
-        Me.tableLayoutPanel2 = New System.Windows.Forms.TableLayoutPanel()
-        Me.btnCancelar = New System.Windows.Forms.Button()
-        Me.btnAceptar = New System.Windows.Forms.Button()
-        Me.tableLayoutPanel1.SuspendLayout()
-        Me.panel1.SuspendLayout()
-        Me.tableLayoutPanel2.SuspendLayout()
+        Me.ListRoles = New System.Windows.Forms.ListBox()
+        Me.TreePermisos = New System.Windows.Forms.TreeView()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.BtnNvoRol = New System.Windows.Forms.Button()
+        Me.BtnGuardar = New System.Windows.Forms.Button()
+        Me.BtnDeshacer = New System.Windows.Forms.Button()
+        Me.Label3 = New System.Windows.Forms.Label()
         Me.SuspendLayout()
         '
-        'tableLayoutPanel1
+        'ListRoles
         '
-        Me.tableLayoutPanel1.ColumnCount = 2
-        Me.tableLayoutPanel1.ColumnStyles.Add(New System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 225.0!))
-        Me.tableLayoutPanel1.ColumnStyles.Add(New System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100.0!))
-        Me.tableLayoutPanel1.Controls.Add(Me.lblNombreFamilia, 0, 1)
-        Me.tableLayoutPanel1.Controls.Add(Me.lblTituloPatentes, 0, 2)
-        Me.tableLayoutPanel1.Controls.Add(Me.panel1, 0, 0)
-        Me.tableLayoutPanel1.Controls.Add(Me.txtNombre, 1, 1)
-        Me.tableLayoutPanel1.Controls.Add(Me.treePatentes, 1, 2)
-        Me.tableLayoutPanel1.Controls.Add(Me.tableLayoutPanel2, 0, 3)
-        Me.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill
-        Me.tableLayoutPanel1.Location = New System.Drawing.Point(0, 0)
-        Me.tableLayoutPanel1.Name = "tableLayoutPanel1"
-        Me.tableLayoutPanel1.RowCount = 4
-        Me.tableLayoutPanel1.RowStyles.Add(New System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 75.0!))
-        Me.tableLayoutPanel1.RowStyles.Add(New System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 37.0!))
-        Me.tableLayoutPanel1.RowStyles.Add(New System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100.0!))
-        Me.tableLayoutPanel1.RowStyles.Add(New System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 75.0!))
-        Me.tableLayoutPanel1.Size = New System.Drawing.Size(556, 549)
-        Me.tableLayoutPanel1.TabIndex = 2
+        Me.ListRoles.FormattingEnabled = True
+        Me.ListRoles.ItemHeight = 20
+        Me.ListRoles.Location = New System.Drawing.Point(12, 98)
+        Me.ListRoles.Name = "ListRoles"
+        Me.ListRoles.Size = New System.Drawing.Size(236, 424)
+        Me.ListRoles.TabIndex = 0
         '
-        'lblNombreFamilia
+        'TreePermisos
         '
-        Me.lblNombreFamilia.Dock = System.Windows.Forms.DockStyle.Fill
-        Me.lblNombreFamilia.Location = New System.Drawing.Point(3, 75)
-        Me.lblNombreFamilia.Name = "lblNombreFamilia"
-        Me.lblNombreFamilia.Size = New System.Drawing.Size(219, 37)
-        Me.lblNombreFamilia.TabIndex = 0
-        Me.lblNombreFamilia.Text = "Nombre :"
-        Me.lblNombreFamilia.TextAlign = System.Drawing.ContentAlignment.MiddleRight
+        Me.TreePermisos.CheckBoxes = True
+        Me.TreePermisos.Location = New System.Drawing.Point(285, 98)
+        Me.TreePermisos.Name = "TreePermisos"
+        Me.TreePermisos.Size = New System.Drawing.Size(353, 424)
+        Me.TreePermisos.TabIndex = 1
         '
-        'lblTituloPatentes
+        'Label1
         '
-        Me.lblTituloPatentes.Dock = System.Windows.Forms.DockStyle.Fill
-        Me.lblTituloPatentes.Location = New System.Drawing.Point(3, 112)
-        Me.lblTituloPatentes.Name = "lblTituloPatentes"
-        Me.lblTituloPatentes.Size = New System.Drawing.Size(219, 362)
-        Me.lblTituloPatentes.TabIndex = 1
-        Me.lblTituloPatentes.Text = "Patentes :"
-        Me.lblTituloPatentes.TextAlign = System.Drawing.ContentAlignment.MiddleRight
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(12, 75)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(50, 20)
+        Me.Label1.TabIndex = 2
+        Me.Label1.Text = "Roles"
         '
-        'panel1
+        'Label2
         '
-        Me.panel1.BackColor = System.Drawing.SystemColors.ControlLight
-        Me.tableLayoutPanel1.SetColumnSpan(Me.panel1, 2)
-        Me.panel1.Controls.Add(Me.Familia)
-        Me.panel1.Dock = System.Windows.Forms.DockStyle.Fill
-        Me.panel1.Location = New System.Drawing.Point(3, 3)
-        Me.panel1.Name = "panel1"
-        Me.panel1.Size = New System.Drawing.Size(550, 69)
-        Me.panel1.TabIndex = 2
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(281, 75)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(127, 20)
+        Me.Label2.TabIndex = 3
+        Me.Label2.Text = "Permisos del Rol"
         '
-        'Familia
+        'BtnNvoRol
         '
-        Me.Familia.Dock = System.Windows.Forms.DockStyle.Fill
-        Me.Familia.Font = New System.Drawing.Font("Microsoft Sans Serif", 13.8!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.Familia.Location = New System.Drawing.Point(0, 0)
-        Me.Familia.Name = "Familia"
-        Me.Familia.Size = New System.Drawing.Size(550, 69)
-        Me.Familia.TabIndex = 0
-        Me.Familia.Text = "Creación de Roles"
-        Me.Familia.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        Me.BtnNvoRol.Location = New System.Drawing.Point(12, 528)
+        Me.BtnNvoRol.Name = "BtnNvoRol"
+        Me.BtnNvoRol.Size = New System.Drawing.Size(146, 32)
+        Me.BtnNvoRol.TabIndex = 4
+        Me.BtnNvoRol.Text = "Nuevo Rol"
+        Me.BtnNvoRol.UseVisualStyleBackColor = True
         '
-        'txtNombre
+        'BtnGuardar
         '
-        Me.txtNombre.Dock = System.Windows.Forms.DockStyle.Fill
-        Me.txtNombre.Location = New System.Drawing.Point(228, 78)
-        Me.txtNombre.Name = "txtNombre"
-        Me.txtNombre.Size = New System.Drawing.Size(325, 26)
-        Me.txtNombre.TabIndex = 3
+        Me.BtnGuardar.Location = New System.Drawing.Point(285, 528)
+        Me.BtnGuardar.Name = "BtnGuardar"
+        Me.BtnGuardar.Size = New System.Drawing.Size(146, 32)
+        Me.BtnGuardar.TabIndex = 5
+        Me.BtnGuardar.Text = "Guardar cambios"
+        Me.BtnGuardar.UseVisualStyleBackColor = True
         '
-        'treePatentes
+        'BtnDeshacer
         '
-        Me.treePatentes.CheckBoxes = True
-        Me.treePatentes.Dock = System.Windows.Forms.DockStyle.Fill
-        Me.treePatentes.Location = New System.Drawing.Point(228, 115)
-        Me.treePatentes.Name = "treePatentes"
-        Me.treePatentes.Size = New System.Drawing.Size(325, 356)
-        Me.treePatentes.TabIndex = 4
+        Me.BtnDeshacer.Location = New System.Drawing.Point(437, 528)
+        Me.BtnDeshacer.Name = "BtnDeshacer"
+        Me.BtnDeshacer.Size = New System.Drawing.Size(164, 32)
+        Me.BtnDeshacer.TabIndex = 6
+        Me.BtnDeshacer.Text = "Deshacer cambios"
+        Me.BtnDeshacer.UseVisualStyleBackColor = True
         '
-        'tableLayoutPanel2
+        'Label3
         '
-        Me.tableLayoutPanel2.ColumnCount = 2
-        Me.tableLayoutPanel1.SetColumnSpan(Me.tableLayoutPanel2, 2)
-        Me.tableLayoutPanel2.ColumnStyles.Add(New System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50.0!))
-        Me.tableLayoutPanel2.ColumnStyles.Add(New System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50.0!))
-        Me.tableLayoutPanel2.Controls.Add(Me.btnCancelar, 1, 0)
-        Me.tableLayoutPanel2.Controls.Add(Me.btnAceptar, 0, 0)
-        Me.tableLayoutPanel2.Dock = System.Windows.Forms.DockStyle.Fill
-        Me.tableLayoutPanel2.Location = New System.Drawing.Point(3, 477)
-        Me.tableLayoutPanel2.Name = "tableLayoutPanel2"
-        Me.tableLayoutPanel2.RowCount = 1
-        Me.tableLayoutPanel2.RowStyles.Add(New System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50.0!))
-        Me.tableLayoutPanel2.Size = New System.Drawing.Size(550, 69)
-        Me.tableLayoutPanel2.TabIndex = 5
-        '
-        'btnCancelar
-        '
-        Me.btnCancelar.Dock = System.Windows.Forms.DockStyle.Fill
-        Me.btnCancelar.Location = New System.Drawing.Point(278, 3)
-        Me.btnCancelar.Name = "btnCancelar"
-        Me.btnCancelar.Size = New System.Drawing.Size(269, 63)
-        Me.btnCancelar.TabIndex = 0
-        Me.btnCancelar.Text = "Cancelar"
-        Me.btnCancelar.UseVisualStyleBackColor = True
-        '
-        'btnAceptar
-        '
-        Me.btnAceptar.Dock = System.Windows.Forms.DockStyle.Fill
-        Me.btnAceptar.Location = New System.Drawing.Point(3, 3)
-        Me.btnAceptar.Name = "btnAceptar"
-        Me.btnAceptar.Size = New System.Drawing.Size(269, 63)
-        Me.btnAceptar.TabIndex = 1
-        Me.btnAceptar.Text = "Aceptar"
-        Me.btnAceptar.UseVisualStyleBackColor = True
+        Me.Label3.AutoSize = True
+        Me.Label3.Font = New System.Drawing.Font("Microsoft Sans Serif", 16.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label3.Location = New System.Drawing.Point(177, 9)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(272, 37)
+        Me.Label3.TabIndex = 8
+        Me.Label3.Text = "Roles del Sistema"
         '
         'FormRoles
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(9.0!, 20.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(556, 549)
-        Me.Controls.Add(Me.tableLayoutPanel1)
+        Me.ClientSize = New System.Drawing.Size(647, 577)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.BtnDeshacer)
+        Me.Controls.Add(Me.BtnGuardar)
+        Me.Controls.Add(Me.BtnNvoRol)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.TreePermisos)
+        Me.Controls.Add(Me.ListRoles)
         Me.Margin = New System.Windows.Forms.Padding(4, 5, 4, 5)
         Me.Name = "FormRoles"
         Me.Text = "FormRoles"
-        Me.tableLayoutPanel1.ResumeLayout(False)
-        Me.tableLayoutPanel1.PerformLayout()
-        Me.panel1.ResumeLayout(False)
-        Me.tableLayoutPanel2.ResumeLayout(False)
         Me.ResumeLayout(False)
+        Me.PerformLayout()
 
     End Sub
 
-    Private WithEvents tableLayoutPanel1 As TableLayoutPanel
-    Private WithEvents lblNombreFamilia As Label
-    Private WithEvents lblTituloPatentes As Label
-    Private WithEvents panel1 As Panel
-    Private WithEvents Familia As Label
-    Private WithEvents txtNombre As TextBox
-    Private WithEvents treePatentes As TreeView
-    Private WithEvents tableLayoutPanel2 As TableLayoutPanel
-    Private WithEvents btnCancelar As Button
-    Private WithEvents btnAceptar As Button
+    Friend WithEvents ListRoles As ListBox
+    Friend WithEvents TreePermisos As TreeView
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents BtnNvoRol As Button
+    Friend WithEvents BtnGuardar As Button
+    Friend WithEvents BtnDeshacer As Button
+    Friend WithEvents Label3 As Label
 End Class
--- Campo2017_VentaPasajes/Seguridad/Roles/FormRoles.vb
+++ Campo2017_VentaPasajes/Seguridad/Roles/FormRoles.vb
@@ -1,43 +1,173 @@
 ﻿Imports BLL
 
 Public Class FormRoles
-    Private Sub btnAceptar_Click(sender As Object, e As EventArgs) Handles btnAceptar.Click
-        Dim mRol As New RolBLL
-        Dim mListaPermisos As New List(Of PermisoAbstractoBLL)
+    Dim mRolSelec As RolBLL
+    Dim mEstadoOriginal As TreeNodeCollection 'Contiene el estado de los nodos del treeview antes de que se cambie
 
-        mRol.Nombre = txtNombre.Text
-        GetPermisosSeleccionados(treePatentes.Nodes, mListaPermisos)
-        mRol.ListaPermisos = mListaPermisos
-        mRol.Guardar()
+#Region "Eventos Form"
+    Private Sub FormRoles_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        'BtnGuardar.Enabled = False
+        'BtnDeshacer.Enabled = False
 
-        Me.Close()
+        ActualizarRoles()
     End Sub
 
 
-    Public Sub GetPermisosSeleccionados(pNodos As TreeNodeCollection, pListaPermisos As List(Of PermisoAbstractoBLL))
-        For Each mNode As TreeNode In pNodos
-            If mNode.Checked Then
-                pListaPermisos.Add(CType(mNode.Tag, PermisoAbstractoBLL))
-            End If
+    Private Sub ListRoles_SelectedValueChanged(sender As Object, e As EventArgs) Handles ListRoles.SelectedValueChanged
+        If Not IsNothing(ListRoles.SelectedItem) And TypeOf (ListRoles.SelectedItem) Is RolBLL Then
+            mRolSelec = ListRoles.SelectedItem
+            ActualizarPermisos()
+        End If
+    End Sub
 
-            GetPermisosSeleccionados(mNode.Nodes, pListaPermisos)
+
+    Private Sub TreePermisos_AfterCheck(sender As Object, e As TreeViewEventArgs) Handles TreePermisos.AfterCheck
+
+    End Sub
+#End Region
+
+
+#Region "Botones"
+    Private Sub BtnNvoRol_Click(sender As Object, e As EventArgs) Handles BtnNvoRol.Click
+        Dim mRol As New RolBLL
+
+        mRol.Nombre = InputBox("Ingrese el nombre del rol:")
+
+        mRol.Guardar()
+
+        ActualizarRoles()
+    End Sub
+#End Region
+
+
+    Public Sub ActualizarRoles()
+        ListRoles.DataSource = Nothing
+        ListRoles.DataSource = RolBLL.ListarRoles
+
+        GuardarEstado()
+    End Sub
+
+
+    Public Sub ActualizarPermisos()
+        If Not IsNothing(mRolSelec) Then
+            TreePermisos.Nodes.Clear()
+
+            Dim mPermisoRaiz As New PermisoCompuestoBLL(0)
+            mPermisoRaiz.MostrarEnTreeview(Me.TreePermisos)
+
+            'Compara el contenido de cada nodo con cada permiso que haya en la lista de permisos del rol
+            'para marcarlos como seleccionados
+            For Each mNodo As TreeNode In TreePermisos.Nodes
+                For Each mPermiso As PermisoAbstractoBLL In mRolSelec.ListaPermisos
+                    SeleccionarNodos(mNodo, mPermiso)
+                Next
+            Next
+
+            TreePermisos.ExpandAll()
+        End If
+    End Sub
+
+
+    ''' <summary>
+    ''' Selecciona todos los nodos que tengan los permisos que hay en el rol
+    ''' </summary>
+    ''' <param name="pNodo">Nodo que contiene el permiso a comparar</param>
+    ''' <param name="pPermiso">Permiso del rol que se compara con el contenido del nodo</param>
+    Public Sub SeleccionarNodos(pNodo As TreeNode, pPermiso As PermisoAbstractoBLL)
+        If TypeOf (pNodo.Tag) Is PermisoCompuestoBLL Then
+            If CType(pNodo.Tag, PermisoAbstractoBLL).Nombre = pPermiso.Nombre Then
+                SeleccionNodoRaiz(pNodo)
+            Else
+                For Each mNodo As TreeNode In pNodo.Nodes
+                    SeleccionarNodos(mNodo, pPermiso)
+                Next
+            End If
+        ElseIf TypeOf (pNodo.Tag) Is PermisoBLL Then
+            If CType(pNodo.Tag, PermisoAbstractoBLL).Nombre = pPermiso.Nombre Then
+                pNodo.Checked = True
+            End If
+        End If
+    End Sub
+
+
+    ''' <summary>
+    ''' Marca como seleccionado aun nodo y todos los nodos que dependan de este. Si alguno de esos nodos, tiene
+    ''' mas nodos, tambien los selecciona
+    ''' </summary>
+    ''' <param name="pNodo">Nodo con el PermisoCompuesto que se quiere seleccionar</param>
+    Public Sub SeleccionNodoRaiz(pNodo As TreeNode)
+        pNodo.Checked = True
+
+        For Each mNodo As TreeNode In pNodo.Nodes
+            SeleccionNodoRaiz(mNodo)
         Next
     End Sub
 
 
-    Private Sub treePatentes_AfterCheck(sender As Object, e As TreeViewEventArgs) Handles treePatentes.AfterCheck
-        e.Node.Tag.Seleccionada = e.Node.Checked
+    ''' <summary>
+    ''' Guarda el estado de los nodos del treeview en caso de que se quieran deshacer cambios
+    ''' </summary>
+    Public Sub GuardarEstado()
+        mEstadoOriginal = TreePermisos.Nodes
+    End Sub
+
+
+    ''' <summary>
+    ''' Vuelve a cargar en el treeview los nodos de la forma en que estaban antes de realizar los cambios
+    ''' </summary>
+    Public Sub RecuperarEstado()
+        TreePermisos.Nodes.Clear()
+
+        For Each mNodo As TreeNode In mEstadoOriginal
+            TreePermisos.Nodes.Add(mNodo)
+        Next
     End Sub
 
 
 
-    Private Sub btnCancelar_Click(sender As Object, e As EventArgs) Handles btnCancelar.Click
-        Me.Close()
-    End Sub
 
 
-    Private Sub FormRoles_Load(sender As Object, e As EventArgs) Handles MyBase.Load
-        Dim mPermisoRaiz As New PermisoCompuestoBLL(0)
-        mPermisoRaiz.MostrarEnTreeview(Me.treePatentes)
-    End Sub
+
+
+
+
+    'Private Sub btnAceptar_Click(sender As Object, e As EventArgs) Handles btnAceptar.Click
+    '    Dim mRol As New RolBLL
+    '    Dim mListaPermisos As New List(Of PermisoAbstractoBLL)
+
+    '    mRol.Nombre = txtNombre.Text
+    '    GetPermisosSeleccionados(treePatentes.Nodes, mListaPermisos)
+    '    mRol.ListaPermisos = mListaPermisos
+    '    mRol.Guardar()
+
+    '    Me.Close()
+    'End Sub
+
+
+    'Public Sub GetPermisosSeleccionados(pNodos As TreeNodeCollection, pListaPermisos As List(Of PermisoAbstractoBLL))
+    '    For Each mNode As TreeNode In pNodos
+    '        If mNode.Checked Then
+    '            pListaPermisos.Add(CType(mNode.Tag, PermisoAbstractoBLL))
+    '        End If
+
+    '        GetPermisosSeleccionados(mNode.Nodes, pListaPermisos)
+    '    Next
+    'End Sub
+
+
+    'Private Sub treePatentes_AfterCheck(sender As Object, e As TreeViewEventArgs) Handles treePatentes.AfterCheck
+    '    e.Node.Tag.Seleccionada = e.Node.Checked
+    'End Sub
+
+
+
+    'Private Sub btnCancelar_Click(sender As Object, e As EventArgs) Handles btnCancelar.Click
+    '    Me.Close()
+    'End Sub
+
+
+    'Private Sub FormRoles_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+    '    Dim mPermisoRaiz As New PermisoCompuestoBLL(0)
+    '    mPermisoRaiz.MostrarEnTreeview(Me.treePatentes)
+    'End Sub
 End Class
