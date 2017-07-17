124841442a41315f65d4ec489ca5e48c3aaf4ffd
Pablo Germone <pabgermone@gmail.com>
2017-07-17 03:39:06 -0300

    Cambios en Forms

--- Campo2017_VentaPasajes/Negocio/ABMs/Venta/FormVentas.Designer.vb
+++ Campo2017_VentaPasajes/Negocio/ABMs/Venta/FormVentas.Designer.vb
@@ -68,11 +68,12 @@ Partial Class FormVentas
         Me.TabControlVentas.Controls.Add(Me.TabVuelos)
         Me.TabControlVentas.Controls.Add(Me.TabPasajeros)
         Me.TabControlVentas.Controls.Add(Me.TabAsientos)
-        Me.TabControlVentas.ItemSize = New System.Drawing.Size(50, 10)
+        Me.TabControlVentas.ItemSize = New System.Drawing.Size(0, 1)
         Me.TabControlVentas.Location = New System.Drawing.Point(0, 0)
+        Me.TabControlVentas.Margin = New System.Windows.Forms.Padding(2, 2, 2, 2)
         Me.TabControlVentas.Name = "TabControlVentas"
         Me.TabControlVentas.SelectedIndex = 0
-        Me.TabControlVentas.Size = New System.Drawing.Size(715, 597)
+        Me.TabControlVentas.Size = New System.Drawing.Size(477, 388)
         Me.TabControlVentas.SizeMode = System.Windows.Forms.TabSizeMode.Fixed
         Me.TabControlVentas.TabIndex = 0
         '
@@ -82,19 +83,21 @@ Partial Class FormVentas
         Me.TabVuelos.Controls.Add(Me.Label1)
         Me.TabVuelos.Controls.Add(Me.TxtBusqueda)
         Me.TabVuelos.Controls.Add(Me.GridVuelos)
-        Me.TabVuelos.Location = New System.Drawing.Point(4, 14)
+        Me.TabVuelos.Location = New System.Drawing.Point(4, 5)
+        Me.TabVuelos.Margin = New System.Windows.Forms.Padding(2, 2, 2, 2)
         Me.TabVuelos.Name = "TabVuelos"
-        Me.TabVuelos.Padding = New System.Windows.Forms.Padding(3)
-        Me.TabVuelos.Size = New System.Drawing.Size(707, 579)
+        Me.TabVuelos.Padding = New System.Windows.Forms.Padding(2, 2, 2, 2)
+        Me.TabVuelos.Size = New System.Drawing.Size(469, 379)
         Me.TabVuelos.TabIndex = 0
         Me.TabVuelos.Text = "TabPage1"
         Me.TabVuelos.UseVisualStyleBackColor = True
         '
         'BtnSiguienteTab1
         '
-        Me.BtnSiguienteTab1.Location = New System.Drawing.Point(574, 541)
+        Me.BtnSiguienteTab1.Location = New System.Drawing.Point(383, 352)
+        Me.BtnSiguienteTab1.Margin = New System.Windows.Forms.Padding(2, 2, 2, 2)
         Me.BtnSiguienteTab1.Name = "BtnSiguienteTab1"
-        Me.BtnSiguienteTab1.Size = New System.Drawing.Size(126, 33)
+        Me.BtnSiguienteTab1.Size = New System.Drawing.Size(84, 21)
         Me.BtnSiguienteTab1.TabIndex = 3
         Me.BtnSiguienteTab1.Text = "Siguiente"
         Me.BtnSiguienteTab1.UseVisualStyleBackColor = True
@@ -102,26 +105,29 @@ Partial Class FormVentas
         'Label1
         '
         Me.Label1.AutoSize = True
-        Me.Label1.Location = New System.Drawing.Point(6, 3)
+        Me.Label1.Location = New System.Drawing.Point(4, 2)
+        Me.Label1.Margin = New System.Windows.Forms.Padding(2, 0, 2, 0)
         Me.Label1.Name = "Label1"
-        Me.Label1.Size = New System.Drawing.Size(59, 20)
+        Me.Label1.Size = New System.Drawing.Size(40, 13)
         Me.Label1.TabIndex = 2
         Me.Label1.Text = "Buscar"
         '
         'TxtBusqueda
         '
-        Me.TxtBusqueda.Location = New System.Drawing.Point(8, 26)
+        Me.TxtBusqueda.Location = New System.Drawing.Point(5, 17)
+        Me.TxtBusqueda.Margin = New System.Windows.Forms.Padding(2, 2, 2, 2)
         Me.TxtBusqueda.Name = "TxtBusqueda"
-        Me.TxtBusqueda.Size = New System.Drawing.Size(347, 26)
+        Me.TxtBusqueda.Size = New System.Drawing.Size(233, 20)
         Me.TxtBusqueda.TabIndex = 1
         '
         'GridVuelos
         '
         Me.GridVuelos.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
-        Me.GridVuelos.Location = New System.Drawing.Point(8, 58)
+        Me.GridVuelos.Location = New System.Drawing.Point(5, 38)
+        Me.GridVuelos.Margin = New System.Windows.Forms.Padding(2, 2, 2, 2)
         Me.GridVuelos.Name = "GridVuelos"
         Me.GridVuelos.RowTemplate.Height = 28
-        Me.GridVuelos.Size = New System.Drawing.Size(692, 479)
+        Me.GridVuelos.Size = New System.Drawing.Size(461, 311)
         Me.GridVuelos.TabIndex = 0
         '
         'TabPasajeros
@@ -134,27 +140,30 @@ Partial Class FormVentas
         Me.TabPasajeros.Controls.Add(Me.BtnAnteriorTab2)
         Me.TabPasajeros.Controls.Add(Me.BtnSiguienteTab2)
         Me.TabPasajeros.Location = New System.Drawing.Point(4, 14)
+        Me.TabPasajeros.Margin = New System.Windows.Forms.Padding(2, 2, 2, 2)
         Me.TabPasajeros.Name = "TabPasajeros"
-        Me.TabPasajeros.Padding = New System.Windows.Forms.Padding(3)
-        Me.TabPasajeros.Size = New System.Drawing.Size(707, 579)
+        Me.TabPasajeros.Padding = New System.Windows.Forms.Padding(2, 2, 2, 2)
+        Me.TabPasajeros.Size = New System.Drawing.Size(469, 370)
         Me.TabPasajeros.TabIndex = 1
         Me.TabPasajeros.Text = "TabPage2"
         Me.TabPasajeros.UseVisualStyleBackColor = True
         '
         'BtnRegistrar
         '
-        Me.BtnRegistrar.Location = New System.Drawing.Point(8, 433)
+        Me.BtnRegistrar.Location = New System.Drawing.Point(5, 281)
+        Me.BtnRegistrar.Margin = New System.Windows.Forms.Padding(2, 2, 2, 2)
         Me.BtnRegistrar.Name = "BtnRegistrar"
-        Me.BtnRegistrar.Size = New System.Drawing.Size(220, 33)
+        Me.BtnRegistrar.Size = New System.Drawing.Size(147, 21)
         Me.BtnRegistrar.TabIndex = 11
         Me.BtnRegistrar.Text = "Registrar nuevo Pasajero"
         Me.BtnRegistrar.UseVisualStyleBackColor = True
         '
         'BtnAgregar
         '
-        Me.BtnAgregar.Location = New System.Drawing.Point(8, 485)
+        Me.BtnAgregar.Location = New System.Drawing.Point(5, 315)
+        Me.BtnAgregar.Margin = New System.Windows.Forms.Padding(2, 2, 2, 2)
         Me.BtnAgregar.Name = "BtnAgregar"
-        Me.BtnAgregar.Size = New System.Drawing.Size(220, 33)
+        Me.BtnAgregar.Size = New System.Drawing.Size(147, 21)
         Me.BtnAgregar.TabIndex = 10
         Me.BtnAgregar.Text = "Agregar pasajero"
         Me.BtnAgregar.UseVisualStyleBackColor = True
@@ -174,27 +183,31 @@ Partial Class FormVentas
         Me.GroupBox1.Controls.Add(Me.TxtApellido)
         Me.GroupBox1.Controls.Add(Me.Label2)
         Me.GroupBox1.Controls.Add(Me.TxtNombre)
-        Me.GroupBox1.Location = New System.Drawing.Point(8, 40)
+        Me.GroupBox1.Location = New System.Drawing.Point(5, 26)
+        Me.GroupBox1.Margin = New System.Windows.Forms.Padding(2, 2, 2, 2)
         Me.GroupBox1.Name = "GroupBox1"
-        Me.GroupBox1.Size = New System.Drawing.Size(302, 378)
+        Me.GroupBox1.Padding = New System.Windows.Forms.Padding(2, 2, 2, 2)
+        Me.GroupBox1.Size = New System.Drawing.Size(201, 246)
         Me.GroupBox1.TabIndex = 9
         Me.GroupBox1.TabStop = False
         '
         'Label7
         '
         Me.Label7.AutoSize = True
-        Me.Label7.Location = New System.Drawing.Point(6, 137)
+        Me.Label7.Location = New System.Drawing.Point(4, 89)
+        Me.Label7.Margin = New System.Windows.Forms.Padding(2, 0, 2, 0)
         Me.Label7.Name = "Label7"
-        Me.Label7.Size = New System.Drawing.Size(45, 20)
+        Me.Label7.Size = New System.Drawing.Size(31, 13)
         Me.Label7.TabIndex = 12
         Me.Label7.Text = "Sexo"
         '
         'RadioFemenino
         '
         Me.RadioFemenino.AutoSize = True
-        Me.RadioFemenino.Location = New System.Drawing.Point(98, 160)
+        Me.RadioFemenino.Location = New System.Drawing.Point(65, 104)
+        Me.RadioFemenino.Margin = New System.Windows.Forms.Padding(2, 2, 2, 2)
         Me.RadioFemenino.Name = "RadioFemenino"
-        Me.RadioFemenino.Size = New System.Drawing.Size(44, 24)
+        Me.RadioFemenino.Size = New System.Drawing.Size(31, 17)
         Me.RadioFemenino.TabIndex = 11
         Me.RadioFemenino.TabStop = True
         Me.RadioFemenino.Text = "F"
@@ -203,9 +216,10 @@ Partial Class FormVentas
         'RadioMasculino
         '
         Me.RadioMasculino.AutoSize = True
-        Me.RadioMasculino.Location = New System.Drawing.Point(10, 160)
+        Me.RadioMasculino.Location = New System.Drawing.Point(7, 104)
+        Me.RadioMasculino.Margin = New System.Windows.Forms.Padding(2, 2, 2, 2)
         Me.RadioMasculino.Name = "RadioMasculino"
-        Me.RadioMasculino.Size = New System.Drawing.Size(47, 24)
+        Me.RadioMasculino.Size = New System.Drawing.Size(34, 17)
         Me.RadioMasculino.TabIndex = 10
         Me.RadioMasculino.TabStop = True
         Me.RadioMasculino.Text = "M"
@@ -214,115 +228,129 @@ Partial Class FormVentas
         'Label6
         '
         Me.Label6.AutoSize = True
-        Me.Label6.Location = New System.Drawing.Point(6, 315)
+        Me.Label6.Location = New System.Drawing.Point(4, 205)
+        Me.Label6.Margin = New System.Windows.Forms.Padding(2, 0, 2, 0)
         Me.Label6.Name = "Label6"
-        Me.Label6.Size = New System.Drawing.Size(82, 20)
+        Me.Label6.Size = New System.Drawing.Size(55, 13)
         Me.Label6.TabIndex = 9
         Me.Label6.Text = "Pasaporte"
         '
         'TxtPasaporte
         '
-        Me.TxtPasaporte.Location = New System.Drawing.Point(12, 338)
+        Me.TxtPasaporte.Location = New System.Drawing.Point(8, 220)
+        Me.TxtPasaporte.Margin = New System.Windows.Forms.Padding(2, 2, 2, 2)
         Me.TxtPasaporte.Name = "TxtPasaporte"
-        Me.TxtPasaporte.Size = New System.Drawing.Size(187, 26)
+        Me.TxtPasaporte.Size = New System.Drawing.Size(126, 20)
         Me.TxtPasaporte.TabIndex = 8
         '
         'Label5
         '
         Me.Label5.AutoSize = True
-        Me.Label5.Location = New System.Drawing.Point(6, 261)
+        Me.Label5.Location = New System.Drawing.Point(4, 170)
+        Me.Label5.Margin = New System.Windows.Forms.Padding(2, 0, 2, 0)
         Me.Label5.Name = "Label5"
-        Me.Label5.Size = New System.Drawing.Size(37, 20)
+        Me.Label5.Size = New System.Drawing.Size(26, 13)
         Me.Label5.TabIndex = 7
         Me.Label5.Text = "DNI"
         '
         'TxtDNI
         '
-        Me.TxtDNI.Location = New System.Drawing.Point(12, 284)
+        Me.TxtDNI.Location = New System.Drawing.Point(8, 185)
+        Me.TxtDNI.Margin = New System.Windows.Forms.Padding(2, 2, 2, 2)
         Me.TxtDNI.Name = "TxtDNI"
-        Me.TxtDNI.Size = New System.Drawing.Size(187, 26)
+        Me.TxtDNI.Size = New System.Drawing.Size(126, 20)
         Me.TxtDNI.TabIndex = 6
         '
         'Label4
         '
         Me.Label4.AutoSize = True
-        Me.Label4.Location = New System.Drawing.Point(6, 204)
+        Me.Label4.Location = New System.Drawing.Point(4, 133)
+        Me.Label4.Margin = New System.Windows.Forms.Padding(2, 0, 2, 0)
         Me.Label4.Name = "Label4"
-        Me.Label4.Size = New System.Drawing.Size(159, 20)
+        Me.Label4.Size = New System.Drawing.Size(108, 13)
         Me.Label4.TabIndex = 5
         Me.Label4.Text = "Fecha de Nacimiento"
         '
         'TxtFechaNac
         '
-        Me.TxtFechaNac.Location = New System.Drawing.Point(12, 227)
+        Me.TxtFechaNac.Location = New System.Drawing.Point(8, 148)
+        Me.TxtFechaNac.Margin = New System.Windows.Forms.Padding(2, 2, 2, 2)
         Me.TxtFechaNac.Name = "TxtFechaNac"
-        Me.TxtFechaNac.Size = New System.Drawing.Size(187, 26)
+        Me.TxtFechaNac.Size = New System.Drawing.Size(126, 20)
         Me.TxtFechaNac.TabIndex = 4
         '
         'Label3
         '
         Me.Label3.AutoSize = True
-        Me.Label3.Location = New System.Drawing.Point(6, 72)
+        Me.Label3.Location = New System.Drawing.Point(4, 47)
+        Me.Label3.Margin = New System.Windows.Forms.Padding(2, 0, 2, 0)
         Me.Label3.Name = "Label3"
-        Me.Label3.Size = New System.Drawing.Size(65, 20)
+        Me.Label3.Size = New System.Drawing.Size(44, 13)
         Me.Label3.TabIndex = 3
         Me.Label3.Text = "Apellido"
         '
         'TxtApellido
         '
-        Me.TxtApellido.Location = New System.Drawing.Point(12, 95)
+        Me.TxtApellido.Location = New System.Drawing.Point(8, 62)
+        Me.TxtApellido.Margin = New System.Windows.Forms.Padding(2, 2, 2, 2)
         Me.TxtApellido.Name = "TxtApellido"
-        Me.TxtApellido.Size = New System.Drawing.Size(187, 26)
+        Me.TxtApellido.Size = New System.Drawing.Size(126, 20)
         Me.TxtApellido.TabIndex = 2
         '
         'Label2
         '
         Me.Label2.AutoSize = True
-        Me.Label2.Location = New System.Drawing.Point(6, 13)
+        Me.Label2.Location = New System.Drawing.Point(4, 8)
+        Me.Label2.Margin = New System.Windows.Forms.Padding(2, 0, 2, 0)
         Me.Label2.Name = "Label2"
-        Me.Label2.Size = New System.Drawing.Size(65, 20)
+        Me.Label2.Size = New System.Drawing.Size(44, 13)
         Me.Label2.TabIndex = 1
         Me.Label2.Text = "Nombre"
         '
         'TxtNombre
         '
-        Me.TxtNombre.Location = New System.Drawing.Point(12, 36)
+        Me.TxtNombre.Location = New System.Drawing.Point(8, 23)
+        Me.TxtNombre.Margin = New System.Windows.Forms.Padding(2, 2, 2, 2)
         Me.TxtNombre.Name = "TxtNombre"
-        Me.TxtNombre.Size = New System.Drawing.Size(187, 26)
+        Me.TxtNombre.Size = New System.Drawing.Size(126, 20)
         Me.TxtNombre.TabIndex = 0
         '
         'ComboClientes
         '
         Me.ComboClientes.FormattingEnabled = True
         Me.ComboClientes.Items.AddRange(New Object() {"- Seleccione un pasajero -"})
-        Me.ComboClientes.Location = New System.Drawing.Point(8, 6)
+        Me.ComboClientes.Location = New System.Drawing.Point(5, 4)
+        Me.ComboClientes.Margin = New System.Windows.Forms.Padding(2, 2, 2, 2)
         Me.ComboClientes.Name = "ComboClientes"
-        Me.ComboClientes.Size = New System.Drawing.Size(302, 28)
+        Me.ComboClientes.Size = New System.Drawing.Size(203, 21)
         Me.ComboClientes.TabIndex = 1
         '
         'GridPasajeros
         '
         Me.GridPasajeros.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
-        Me.GridPasajeros.Location = New System.Drawing.Point(332, 6)
+        Me.GridPasajeros.Location = New System.Drawing.Point(221, 4)
+        Me.GridPasajeros.Margin = New System.Windows.Forms.Padding(2, 2, 2, 2)
         Me.GridPasajeros.Name = "GridPasajeros"
         Me.GridPasajeros.RowTemplate.Height = 28
-        Me.GridPasajeros.Size = New System.Drawing.Size(368, 513)
+        Me.GridPasajeros.Size = New System.Drawing.Size(245, 333)
         Me.GridPasajeros.TabIndex = 6
         '
         'BtnAnteriorTab2
         '
-        Me.BtnAnteriorTab2.Location = New System.Drawing.Point(6, 541)
+        Me.BtnAnteriorTab2.Location = New System.Drawing.Point(4, 352)
+        Me.BtnAnteriorTab2.Margin = New System.Windows.Forms.Padding(2, 2, 2, 2)
         Me.BtnAnteriorTab2.Name = "BtnAnteriorTab2"
-        Me.BtnAnteriorTab2.Size = New System.Drawing.Size(126, 33)
+        Me.BtnAnteriorTab2.Size = New System.Drawing.Size(84, 21)
         Me.BtnAnteriorTab2.TabIndex = 5
         Me.BtnAnteriorTab2.Text = "Anterior"
         Me.BtnAnteriorTab2.UseVisualStyleBackColor = True
         '
         'BtnSiguienteTab2
         '
-        Me.BtnSiguienteTab2.Location = New System.Drawing.Point(574, 541)
+        Me.BtnSiguienteTab2.Location = New System.Drawing.Point(383, 352)
+        Me.BtnSiguienteTab2.Margin = New System.Windows.Forms.Padding(2, 2, 2, 2)
         Me.BtnSiguienteTab2.Name = "BtnSiguienteTab2"
-        Me.BtnSiguienteTab2.Size = New System.Drawing.Size(126, 33)
+        Me.BtnSiguienteTab2.Size = New System.Drawing.Size(84, 21)
         Me.BtnSiguienteTab2.TabIndex = 4
         Me.BtnSiguienteTab2.Text = "Siguiente"
         Me.BtnSiguienteTab2.UseVisualStyleBackColor = True
@@ -333,9 +361,10 @@ Partial Class FormVentas
         Me.TabAsientos.Controls.Add(Me.BtnAnteriorTab3)
         Me.TabAsientos.Controls.Add(Me.BtnFinalizar)
         Me.TabAsientos.Location = New System.Drawing.Point(4, 14)
+        Me.TabAsientos.Margin = New System.Windows.Forms.Padding(2, 2, 2, 2)
         Me.TabAsientos.Name = "TabAsientos"
-        Me.TabAsientos.Padding = New System.Windows.Forms.Padding(3)
-        Me.TabAsientos.Size = New System.Drawing.Size(707, 579)
+        Me.TabAsientos.Padding = New System.Windows.Forms.Padding(2, 2, 2, 2)
+        Me.TabAsientos.Size = New System.Drawing.Size(469, 370)
         Me.TabAsientos.TabIndex = 2
         Me.TabAsientos.Text = "TabPage1"
         Me.TabAsientos.UseVisualStyleBackColor = True
@@ -343,37 +372,41 @@ Partial Class FormVentas
         'Label8
         '
         Me.Label8.AutoSize = True
-        Me.Label8.Location = New System.Drawing.Point(59, 246)
+        Me.Label8.Location = New System.Drawing.Point(39, 160)
+        Me.Label8.Margin = New System.Windows.Forms.Padding(2, 0, 2, 0)
         Me.Label8.Name = "Label8"
-        Me.Label8.Size = New System.Drawing.Size(607, 20)
+        Me.Label8.Size = New System.Drawing.Size(408, 13)
         Me.Label8.TabIndex = 6
         Me.Label8.Text = "En esta ventana se deberian elegir los asientos del vuelo que ocuparan los pasaje" &
     "ros"
         '
         'BtnAnteriorTab3
         '
-        Me.BtnAnteriorTab3.Location = New System.Drawing.Point(6, 541)
+        Me.BtnAnteriorTab3.Location = New System.Drawing.Point(4, 352)
+        Me.BtnAnteriorTab3.Margin = New System.Windows.Forms.Padding(2, 2, 2, 2)
         Me.BtnAnteriorTab3.Name = "BtnAnteriorTab3"
-        Me.BtnAnteriorTab3.Size = New System.Drawing.Size(126, 33)
+        Me.BtnAnteriorTab3.Size = New System.Drawing.Size(84, 21)
         Me.BtnAnteriorTab3.TabIndex = 5
         Me.BtnAnteriorTab3.Text = "Anterior"
         Me.BtnAnteriorTab3.UseVisualStyleBackColor = True
         '
         'BtnFinalizar
         '
-        Me.BtnFinalizar.Location = New System.Drawing.Point(574, 541)
+        Me.BtnFinalizar.Location = New System.Drawing.Point(383, 352)
+        Me.BtnFinalizar.Margin = New System.Windows.Forms.Padding(2, 2, 2, 2)
         Me.BtnFinalizar.Name = "BtnFinalizar"
-        Me.BtnFinalizar.Size = New System.Drawing.Size(126, 33)
+        Me.BtnFinalizar.Size = New System.Drawing.Size(84, 21)
         Me.BtnFinalizar.TabIndex = 4
         Me.BtnFinalizar.Text = "Finalizar"
         Me.BtnFinalizar.UseVisualStyleBackColor = True
         '
         'FormVentas
         '
-        Me.AutoScaleDimensions = New System.Drawing.SizeF(9.0!, 20.0!)
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(716, 596)
+        Me.ClientSize = New System.Drawing.Size(477, 387)
         Me.Controls.Add(Me.TabControlVentas)
+        Me.Margin = New System.Windows.Forms.Padding(2, 2, 2, 2)
         Me.Name = "FormVentas"
         Me.Text = "Form1"
         Me.TabControlVentas.ResumeLayout(False)
