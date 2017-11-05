bf07b4c81616239a85e15c840494f35b8eede6df
Pablo Germone <pabgermone@gmail.com>
2017-11-05 08:18:52 -0300

    Cambios en FormVentas   - CU VentaPasje terminado

--- Campo2017_VentaPasajes/My Project/Application.Designer.vb
+++ Campo2017_VentaPasajes/My Project/Application.Designer.vb
@@ -32,7 +32,7 @@ Namespace My
         
         <Global.System.Diagnostics.DebuggerStepThroughAttribute()>  _
         Protected Overrides Sub OnCreateMainForm()
-            Me.MainForm = Global.InterfazUsuario.FormVentas
+            Me.MainForm = Global.InterfazUsuario.FormBackup
         End Sub
     End Class
 End Namespace
